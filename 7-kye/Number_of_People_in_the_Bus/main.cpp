60c17cd9038fb910375a40a6c99e6839de7e4fce
Kyrylo Smyrnov <94990448+KyryloSmyrnov@users.noreply.github.com>
2022-07-04 18:39:40 +0200

    Update main.cpp

--- 7-kye/Number_of_People_in_the_Bus/main.cpp
+++ 7-kye/Number_of_People_in_the_Bus/main.cpp
@@ -1,1 +1,14 @@
+#include <utility>
+#include <vector>
 
+unsigned int number(const std::vector<std::pair<int, int>>& busStops) {
+  
+  unsigned int total = 0;
+
+  for (auto pair : busStops)
+  {
+    total = total + pair.first - pair.second;
+  }
+
+  return total;
+}
